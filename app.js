b2241fb494fc5e56b46cc8a89c3c8db7ca860566
Lilian Korir <likorir@davidson.edu>
2020-04-15 16:42:39 -0400

    changes

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 // // Node's standard path module
 // // See https://nodejs.org/api/path.html
+'use strict';
+
 let path = require('path');
-
-// // The Express web application framework
-// // See http://expressjs.com/
-let express = require('express');
-
-// // Library for nicer logging of HTTP requests
-// // See https://github.com/expressjs/morgan
+let createError = require('http-errors');
+let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let express = require('express');
 
 let app = express();
 
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = app.get('env');
+}
+
 // Tell Express to load static files from the public/ directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.inProduction = () => app.get('env') === 'production';
+app.inDevelopment = () => app.get('env') === 'development';
+
+if (app.inDevelopment()) {
+  app.use(logger('dev'));
+} else {
+  app.use(logger('combined'));
+}
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Knex is a module used to generate SQL queries
+// See http://knexjs.org/
+let Knex = require('knex');
+
+// Objection is a module used to represent and manipuldate
+// data from a SQL database using JavaScript. It uses connect
+// to generate the appropriate SQL queries.
+// See https://vincit.github.io/objection.js/
+let { Model } = require('objection');
+
+// Tell Knex how to connect to our database
+// See config/database.js
+let dbConfig = require(app.root('knexfile'));
+let knex = Knex(dbConfig[process.env.NODE_ENV]);
+Model.knex(knex);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
 let routes = require('./routes');
 app.use('/', routes);
@@ -30,3 +64,5 @@ app.listen(SERVER_PORT, () => {
   console.log(`    http://localhost:${SERVER_PORT}`);
   console.log();
 });
+
+module.exports = app;
---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost:5432/covid19Charts_development',
+    debug: true,
+    migrations: {
+      tableName: 'knex_migrations',
+    }
+
+
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost:5432/covid19Charts_test',
+    migrations: {
+      tableName: 'knex_migrations',
+    }
+
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+
+
+    pool: {
+      min: 2,
+      max: 10,
+    }
+  },
+};
